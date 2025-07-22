24606301616ff2dfc43164f8be43adc42c41bebc
SerhiiKutovyi <wagenrex@gmail.com>
2025-07-22 20:21:45 +0300

    test hw-3

--- js/task-1.js
+++ js/task-1.js
@@ -15,10 +15,46 @@
 // console.log(fruitsLastElement);
 // console.log(fruits[fruitsLastElement]);
 
-function getLastElementMeta(array) {
-  const lastElement = array.length - 1;
-  const valueElement = array[lastElement];
-  return [lastElement, valueElement];
-}
+// function getLastElementMeta(array) {
+//   const lastElement = array.length - 1;
+//   const valueElement = array[lastElement];
+//   return [lastElement, valueElement];
+// }
 
-console.log(getLastElementMeta(['apple', 'peach', 'pear', 'banana']));
+// console.log(getLastElementMeta(['apple', 'peach', 'pear', 'banana']));
+
+// function getExtremeElements(array) {
+//   return [array[0], array[array.length - 1]];
+// }
+
+// console.log(getExtremeElements(['Earth', 'Mars', 'Venus']));
+// console.log(getExtremeElements(['apple', 'peach', 'pear', 'banana']));
+// const emptyArray = [];
+// const nonEmptyArray = [1, 2, 3];
+
+// console.log(Boolean(emptyArray)); // true
+// console.log(Boolean(nonEmptyArray)); // true
+
+// if (emptyArray) {
+//   console.log('if is in progress');
+// } else {
+//   console.log('else is not performed');
+// }
+
+// if (nonEmptyArray) {
+//   console.log('if is in progress');
+// } else {
+//   console.log('else is not performed');
+// }
+
+// function getLength(array) {
+//   const arr = array.join('').length;
+//   console.log(arr);
+// }
+
+// console.log(getLength(['Mango', 'hurries', 'to', 'the', 'train']));
+
+function calculateEngravingPrice(message, pricePerWord) {
+  return message.split(' ').length * pricePerWord;
+}
+console.log(calculateEngravingPrice('JavaScript is in my blood', 10));
