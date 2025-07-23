cf42926775721bb864331bc006ec8278c99f9df5
SerhiiKutovyi <wagenrex@gmail.com>
2025-07-23 21:58:41 +0300

    test hw-3

--- js/task-1.js
+++ js/task-1.js
@@ -54,7 +54,100 @@
 
 // console.log(getLength(['Mango', 'hurries', 'to', 'the', 'train']));
 
-function calculateEngravingPrice(message, pricePerWord) {
-  return message.split(' ').length * pricePerWord;
-}
-console.log(calculateEngravingPrice('JavaScript is in my blood', 10));
+// function calculateEngravingPrice(message, pricePerWord) {
+//   return message.split(' ').length * pricePerWord;
+// }
+// console.log(calculateEngravingPrice('JavaScript is in my blood', 10));
+
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, fruits.length - 1);
+// const lastThreeEls = fruits.slice(-3);
+
+// console.log(firstTwoEls);
+// console.log(nonExtremeEls);
+// console.log(lastThreeEls);
+
+// const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// const newClients = ['Peach', 'Houston'];
+
+// function getSlice(array, value) {
+//   const index = array.indexOf(value);
+//   return array.slice(0, index + 1);
+// }
+// console.log(getSlice(['Mango', 'Poly', 'Ajax'], 'Poly'));
+// console.log(getSlice(['Mango', 'Poly', 'Ajax'], 'Ajax'));
+// console.log(getSlice(['Mango', 'Poly', 'Ajax'], 'Jacob'));
+
+// function createArrayOfNumbers(min, max) {
+//   let array = [];
+
+//   for (let i = min; i <= max; i += 1) {
+//     array.push(i);
+//   }
+//   return array;
+// }
+
+// console.log(createArrayOfNumbers(1, 3));
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   for (const e of order) {
+//     total += e;
+//   }
+//   return total;
+// }
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+
+// function getEvenNumbers(start, end) {
+//   let total = [];
+//   for (let i = start; i <= end; i++) {
+//     if (i % 2 === 0) {
+//       total.push(i);
+//     }
+//   }
+//   return total;
+// }
+
+// console.log(getEvenNumbers(3, 11));
+
+// function checkStorage(storage, item) {
+//   if (storage.includes(item.toLowerCase())) {
+//     return `${item.toLowerCase()} is available to order!`;
+//   } else {
+//     return 'Sorry! We are out of stock!';
+//   }
+// }
+
+// console.log(checkStorage(['apple', 'plum', 'pear'], 'pLuM'));
+// console.log(checkStorage(['apple', 'plum', 'pear'], 'apple'));
+// console.log(checkStorage(['apple', 'plum', 'pear'], 'pEAr'));
+
+// function getCommonElements(array1, array2) {
+//   let newArr = [];
+
+//   for (let i = 0; i < array1.length; i++) {
+//     if (array2.includes(array1[i])) {
+//       newArr.push(array1[i]);
+//     }
+//   }
+//   return newArr;
+// }
+// console.log(getCommonElements([1, 2, 3], [2, 4]));
+
+// function calculateTotalPrice(order) {
+//   let nam = 0;
+//   for (const element of order) {
+//     nam += element;
+//   }
+//   return nam;
+// }
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+// function sum(a, b) {
+//   console.log(arguments);
+//   return a + b;
+// }
+
+// sum(2, 5);
